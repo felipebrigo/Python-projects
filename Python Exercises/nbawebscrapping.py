ac6f5b4c16ab6588c63f0a9566adcfc89c4352d0
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-05-14 16:14:43 -0300

    WebScrapping - NBA

---
+++ Python Exercises/nbawebscrapping.py
@@ -0,0 +1,29 @@
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import json
+
+url= "https://stats.nba.com/players/traditional/"
+
+option = Options()
+option.headless = True
+driver = webdriver.Chrome()
+driver.get(url)
+driver.quit()
+
+
+# 1 - Catch HTML content through URL
+# 2 - Parsing HTML
+# 3 - Structure data through Dataframe Pandas
+# 4 - Change data to Dictionary
+# 5 - Convert and save JSON file
+
+#PIP:
+# pip install requests2
+# pip install pandas
+# pip install lxml
+# pip install beautifulsoup4
+# pip install selenium
