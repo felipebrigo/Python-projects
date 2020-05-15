d8d71bb606d01fb0e8ea89945b0d46103d28cd26
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-05-15 17:23:55 -0300

    finishing NBA webscrapper project

--- Python Exercises/nbawebscrapping.py
+++ Python Exercises/nbawebscrapping.py
@@ -6,30 +6,41 @@ from selenium.webdriver.chrome.options import Options
 import requests
 import json
 
-url= "https://stats.nba.com/players/traditional/"
+# 1 - Catch HTML content through URL
+url= "https://stats.nba.com/players/traditional/?sort=PLAYER_NAME&dir=1"
 
 option = Options()
 option.headless = True
 driver = webdriver.Chrome()
 driver.get(url)
-time.sleep(10)
+time.sleep(5)
 
-driver.find_element_by_xpath("")
-//div[@class = "nba-stat-table"]//table/thead//tr//th[@data-field="PTS"]
+driver.find_element_by_xpath("//div[@class ='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
 
+element=driver.find_element_by_xpath("//div[@class ='nba-stat-table']//table")
+html_content = element.get_attribute('outerHTML')
 
+print(html_content)
 
-# 1 - Catch HTML content through URL
 # 2 - Parsing HTML
+soup = BeautifulSoup(html_content,'html.parser')
+table = soup.find(name='table')
+
 # 3 - Structure data through Dataframe Pandas
+df_full = pd.read_html(str(table))[0].head(10)
+df=df_full[['Unnamed: 0','PLAYER','TEAM','PTS']]
+df.columns = ['pos','player','team','total']
+print(df)
+
 # 4 - Change data to Dictionary
-# 5 - Convert and save JSON file
+top10ranking = {}
 
-#PIP:
-# pip install requests2
-# pip install pandas
-# pip install lxml
-# pip install beautifulsoup4
-# pip install selenium
-
+top10ranking['points'] = df.to_dict('records')
+print(top10ranking)
 driver.quit()
+
+# 5 - Convert and save JSON file
+js=json.dumps(top10ranking)
+fp=open('ranking.json','w')
+fp.write(js)
+fp.close
