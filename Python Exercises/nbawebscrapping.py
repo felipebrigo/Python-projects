c8315b71b77f453c4fde64700828da5e469601a5
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-05-15 23:27:09 -0300

    Update nbawebscrapping.py

--- Python Exercises/nbawebscrapping.py
+++ Python Exercises/nbawebscrapping.py
@@ -7,35 +7,54 @@ import requests
 import json
 
 # 1 - Catch HTML content through URL
-url= "https://stats.nba.com/players/traditional/?sort=PLAYER_NAME&dir=1"
+url = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
+top10ranking = {}
+
+rankings = {
+    '3points': {'field': 'FG3M', 'label': '3PM'},
+    'points': {'field': 'PTS', 'label': 'PTS'},
+    'assistants': {'field': 'AST', 'label': 'AST'},
+    'rebounds': {'field': 'REB', 'label': 'REB'},
+    'steals': {'field': 'STL', 'label': 'STL'},
+    'blocks': {'field': 'BLK', 'label': 'BLK'},
+}
+
+def buildrank(type):
+    
+    field = rankings[type]['field']
+    label = rankings[type]['label']
+
+    driver.find_element_by_xpath(
+        f"//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='{field}']").click()
+    
+    element=driver.find_element_by_xpath("//div[@class ='nba-stat-table']//table")
+    html_content = element.get_attribute('outerHTML')
+
+    print(html_content)
+
+    # 2 - Parsing HTML
+    soup = BeautifulSoup(html_content,'html.parser')
+    table = soup.find(name='table')
+
+    # 3 - Structure data through Dataframe Pandas
+    df_full = pd.read_html(str(table))[0].head(10)
+    df=df_full[['Unnamed: 0','PLAYER','TEAM',label]]
+    df.columns = ['pos','player','team','total']
+    print(df)
+    
+    return df.to_dict('records')
 
 option = Options()
 option.headless = True
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=option)
 driver.get(url)
 time.sleep(5)
 
-driver.find_element_by_xpath("//div[@class ='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
-
-element=driver.find_element_by_xpath("//div[@class ='nba-stat-table']//table")
-html_content = element.get_attribute('outerHTML')
-
-print(html_content)
-
-# 2 - Parsing HTML
-soup = BeautifulSoup(html_content,'html.parser')
-table = soup.find(name='table')
-
-# 3 - Structure data through Dataframe Pandas
-df_full = pd.read_html(str(table))[0].head(10)
-df=df_full[['Unnamed: 0','PLAYER','TEAM','PTS']]
-df.columns = ['pos','player','team','total']
-print(df)
-
 # 4 - Change data to Dictionary
-top10ranking = {}
+for k in rankings:
+    top10ranking[k] = buildrank(k)
+    
 
-top10ranking['points'] = df.to_dict('records')
 print(top10ranking)
 driver.quit()
 
