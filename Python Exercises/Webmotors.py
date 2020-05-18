dd45b98370572164a91d0d4d265a538bccc40a15
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-05-18 16:56:45 -0300

    Update Webmotors.py

--- Python Exercises/Webmotors.py
+++ Python Exercises/Webmotors.py
@@ -7,20 +7,25 @@ import requests
 import json
 
 # 1 - Catch HTML content through URL
-url = "https://www.webmotors.com.br/carros-usados/sp?estadocidade=S%C3%A3o%20Paulo&tipoveiculo=carros-usados"
+url = "https://www.webmotors.com.br/carros/sp?estadocidade=S%C3%A3o%20Paulo&tipoveiculo=carros&precoate=50000&o=5"
 
 option = Options()
 option.headless = True
 driver = webdriver.Chrome()
-
 driver.get(url)
 time.sleep(5)
-element = driver.find_element_by_xpath("//div[@class='ContainerCardVehicle ']")
-html_content = element.get_attribute('outerHTML')
+driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+time.sleep(5)
+element = driver.find_element_by_xpath("//div[@class='Search-result Search-result--container-right']")
+html_content=element.get_attribute('outerHTML')
 
-print(element)
+#print(html_content)
 
 # 2 - Parsing HTML
+soup=BeautifulSoup(html_content,'html.parser')
+print(soup.get_text())
+for link in soup.find_all('a'):
+    print(link.get('href'))
 # 3 - Structure data through Dataframe Pandas
 # 4 - Change data to Dictionary
 driver.quit()
