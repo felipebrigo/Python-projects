1500939ecd709953b721931e1eb50ff04bc81fa8
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-05-20 14:04:15 -0300

    Update Webmotors.py

--- Python Exercises/Webmotors.py
+++ Python Exercises/Webmotors.py
@@ -2,16 +2,18 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.chrome.options import Options
 import requests
 import json
+import re
 
 # 1 - Catch HTML content through URL
-url = "https://www.webmotors.com.br/carros/sp?estadocidade=S%C3%A3o%20Paulo&tipoveiculo=carros&precoate=50000&o=5"
+url = "https://www.webmotors.com.br/carros/sp?estadocidade=S%C3%A3o%20Paulo&tipoveiculo=carros&precoate=30000&o=5"
 
 option = Options()
 option.headless = True
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get(url)
 time.sleep(5)
 driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
@@ -21,17 +23,26 @@ html_content=element.get_attribute('outerHTML')
 
 # 2 - Parsing HTML
 soup=BeautifulSoup(html_content,'html.parser')
-#print(soup.get_text())
+
+print(soup.prettify())
+
+def has_class_span_defined(tag_span):
+    return tag_span.has_attr('class')
+def has_class_strong_defined(tag_strong):
+    return tag_strong.has_attr('class')
+
 for marca in soup.find_all('h2'):
     print(marca.get_text())
 for modelo in soup.find_all('h3'):
     print(modelo.get_text())
-for valor in soup.find_all('strong class'):
+for valor in soup.find_all(has_class_strong_defined):
     print(valor.get_text())
-for ano in soup.find_all('span class'):
-    print(ano.get_text())
+for ano in soup.find_all(has_class_span_defined):
+    print(ano.get_text())    
 for link in soup.find_all('a'):
     print(link.get('href'))
+
+    
 # 3 - Structure data through Dataframe Pandas
 
 # 4 - Change data to Dictionary
