e1017ee7e1d61a437712d7fd995a05b942403649
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-06-04 18:11:14 -0300

    Update ex45 - Jokempo.py

--- Python Exercises/ex45 - Jokempo.py
+++ Python Exercises/ex45 - Jokempo.py
@@ -11,21 +11,21 @@ def startgame():
     print("---------- ROCK / PAPER / SCISSORS ----------")
     print(emoji.emojize("To play ROCK please press 1 - :punch:", use_aliases=True))
     print(emoji.emojize("To play PAPER please press 2 - :raised_hand:", use_aliases=True))
-    print(emoji.emojize("To play SCISSOR please press 3 - :scissors: /n", use_aliases=True))
+    print(emoji.emojize("To play SCISSOR please press 3 - :scissors:", use_aliases=True))
 
 def choiceanalysis():
-    
-    while (type(check_coice = input("Please enter your choice number - 1 / 2 / 3: ")) != int):
+    checkcoice = input("Please enter your choice number - 1 / 2 / 3: ")
+    while (type(checkcoice) != int):
         print("Please re-type your choice. Only will be accepted numbers 1 for ROCK / 2 for PAPER or 3 for SCISSOR:")
-    while (check_choice != 1) or (check_choice != 2) or (check_choice != 3):
+    while checkchoice != 1 or checkchoice != 2 or checkchoice != 3:
         print("Please re-type your choice. Only will be accepted numbers 1 for ROCK / 2 for PAPER or 3 for SCISSOR :")
-    return check_choice
+    return checkchoice
         
 def choicetext():
     global userchoice    
-    print("Let's play? /n")
-    choiceanalysis()
-    userchoice = check_choice
+    print("Let's play?")
+    checkchoice=choiceanalysis()
+    userchoice = checkchoice
     sleep(1.0)
     print(emoji.emojize("ROCK... :punch:", use_aliases=True))
     sleep(1.0)
