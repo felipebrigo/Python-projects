a6b33eb4b0b61c79847b8377b78242593ed33ee1
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-06-24 20:20:05 -0300

    Update ex62.py

--- Python Exercises/ex62.py
+++ Python Exercises/ex62.py
@@ -1,27 +1,30 @@
 print("-=-=-=-=-=-= Arithmetic Progression =-=-=-=-=-=-")
-start=int(input("Please type starting number:"))
-constant=int(input("Please type the constant of your AP:"))
-counting=10
+start = int(input("Please type starting number:"))
+constant = int(input("Please type the constant of your AP:"))
+counting = 10
 number = []
-number=[start]
-n=1
-qtdenumbers=10
-def ap():
+number = [start]
+n = 1
+qtdenumbers = 10
+
+
+def ap(counting):
     global n
     global start
-    while n<counting:
-        newstart=start+constant
+    while n < counting:
+        newstart = start+constant
         number.append(newstart)
-        start=newstart
-        n+=1  
-    n=0
-    while n<counting:
+        start = newstart
+        n += 1
+    n = 0
+    while n < counting:
         print('{} -> '.format(number[n]), end='')
-        n+=1
+        n += 1
     print("PAUSA")
 
-counting=int(input("QUANTOS TERMOS VOCÊ GOSTARIA DE VER A MAIS?"))
-qtdenumbers=qtdenumbers+counting
-ap(counting)
-if counting==0:
-    print("PROGRESSÃO FINALIZADA COM {} TERMOS MOSTRADOS".format(qtdenumbers))    
+while counting != 0:
+    ap(counting)
+    counting = int(input("QUANTOS TERMOS VOCÊ GOSTARIA DE VER A MAIS?"))
+    qtdenumbers = qtdenumbers+counting
+
+print("PROGRESSÃO FINALIZADA COM {} TERMOS MOSTRADOS".format(qtdenumbers))
