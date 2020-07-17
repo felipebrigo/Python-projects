3aa60e11b92f5af75c2f56e61514fd0910087e65
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-07-17 16:16:37 -0300

    Create headsandtailsprediction.py

---
+++ Python Exercises/headsandtailsprediction.py
@@ -0,0 +1,22 @@
+#Testing prediction in heads and tails using AI and self learning computing 
+#Use random module to flip the coin (0-Heads and 1-Tails) and trying to discover the formula behind the scenes
+def headsandtails():
+    from random import randint
+    flipcoin=randint(0,1)
+    print(flipcoin)
+
+def equilibrium():
+    """This function suggest probability to bet in heads(0) or tails(1) depending on the historical data. 
+    The main driver of this analysis is to maintaining the equivalent 50/50 - Equilibrium - for each side """
+    probheads=probtails=0.00
+    print (f'The probability to find heads (0) is {probheads} and tails (1) is {probtails}')
+
+
+import pandas as pd
+#addressfile=path()
+#historicaldata=pd.read_csv(addressfile)
+answer="Y"
+while answer=="Y" or answer=="y":
+    headsandtails()
+    answer=str(input("Do you want to continue? (Y/N):"))
+print("Thank you for playing our game")
