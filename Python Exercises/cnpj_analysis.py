4d4c8bbe3d9528c903806e25174a51a48287311e
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-08-20 00:44:35 -0300

    update CNPJ big-data project

--- Python Exercises/cnpj_analysis.py
+++ Python Exercises/cnpj_analysis.py
@@ -1,76 +1,95 @@
 import timeit
+textsliced=[]
+header_treatment_list=[]
+data_cnpj_list=[]
+partner_list=[]
+secondary_list=[]
+trailler_list=[]
 
 #Slicing the whole datafile in 1200 characters each time
 def slicing():
-    global data_length
-    global data
-    textsliced=[]
-    a=0
-    '''bunchsize = 1000000
-    bunch = []
-        for line in r:
-            x, y, z = line.split(' ')[:3]
-            bunch.append(line.replace(x,x[:-3]).replace(y,y[:-3]).replace(z,z[:-3]))
-            if len(bunch) == bunchsize:
-                w.writelines(bunch)
-                bunch = []
-        w.writelines(bunch)'''
+    global textsliced
+                    
     with open("/Users/mac/Documents/Lista FCA Leandro/Teste-layout.txt", "r") as r, open("teste.txt", "w") as w:
         r=r.read()
         textsliced=[r[i:i+1200] for i in range(0, len(r), 1200)]
-    #with open(("teste.txt"), 'w') as file:
         w.writelines(textsliced)
-    #return r
-'''    for a in range(a,len(textsliced)):
-        
-        print(textsliced[a],end="")
-    
-    a=0
-    i=0
-    j=1199
-    d=""
-    d=data[i:j]
-    for a in range(0,data_length,1200):
-        d=data[i:j]
-        data_list.append(d)
-        i+=1200
-        j+=1200
-'''
-slicing()
-#print(data_list)
-#print(data[0:100])
-#print(data[-1000:-1])      
-#print(len(r))
-print(timeit.timeit(slicing, number=1))
 
-'''
+def file_type():
+    global textsliced
+    for item in range(0,len(textsliced)):
+        if textsliced[item][0]==0:
+            header_treatment()
+            return textsliced[item]
+        elif textsliced[item][0]==1:
+            data_cnpj()
+            return textsliced[item]
+        elif textsliced[item][0]==2:
+            partner()
+            return textsliced[item]
+        elif textsliced[item][0]==6:
+            secondary()
+            return textsliced[item]
+        elif textsliced[item][0]==9:
+            trailler()
+            return textsliced[item]
+        elif textsliced[item][0]==" ":
+            return data_cnpj(textsliced[item])
+
+
 #Header treatment - 1200 characteres
-def header_treatment():
-    global data
-    header=data[0:1199]
-    #Here head treatment and slices 
-    
+def header_treatment(text):
+    header_treatment_list.append(text)
+
 #Company treatment - 1200 characters    
-def data_cnpj():
-    global data
-    company_data=data[0:1199]
+def data_cnpj(text):
+    data_cnpj_list.append(text)
+    
 
 #Partners treatment - 1200 characters
-def partner():
-    global data
-    partner_data=data[0:1199]
+def partner(text):
+    partner_list.append(text)
 
+#Secondary CNAE
+def secondary(text):
+    secondary_list.append(text)
+    
+#Trailler
+def trailler(text):
+    trailler_list.append(text)
 
 #Import Pandas to re-arrange all list
-
 #Export to csv file
-
+def write_txt_file():
+    print(len(data_cnpj_list))
+    with open("cnpj.txt","w") as cnpjteste:
+        cnpjteste.writelines(data_cnpj_list)
+        
 #Main
 slicing()
-header_treatment()
+file_type()
+write_txt_file()
+
+    
+print(timeit.timeit(slicing, number=1))
+print(timeit.timeit(file_type, number=1))
 
 
-import pprint
-pp=pprint.PrettyPrinter(indent=4)
-pp.pprint(data)
-print(len(data))'''
+
+
+
+
+'''Using Buffer on Slicing() is too Slow (0.06865)
+buffersize = 1000000
+buffer = []
+i=0
+with open("/Users/mac/Documents/Lista FCA Leandro/Teste-layout.txt", "r") as r, open("teste.txt", "w") as w:
+    r=r.read()
+    for text in r:
+        text=r[i:i+1200]
+        i+=1200
+        buffer.append(text)
+        if len(buffer) > buffersize:
+            w.writelines(buffer)
+            buffer = []
+    w.writelines(buffer)'''
