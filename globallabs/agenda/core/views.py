f534efd88d915119bf053dd666d6020e97d7fed1
felipebrigo <55454589+felipebrigo@users.noreply.github.com>
2020-10-30 18:14:53 -0300

    updated agenda

--- globallabs/agenda/core/views.py
+++ globallabs/agenda/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 '''Create your views here.
 def index (request):
@@ -7,8 +10,31 @@ def index (request):
 '''
 
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou Senha inválidos!")
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
